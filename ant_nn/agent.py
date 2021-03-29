da77b7bd78111d7a3def88790c925d5f3a391ea9
Elijah Roussos <elijah.rou@gmail.com>
2021-03-29 15:28:16 -0400

    summoreeee

--- ant_nn/agent.py
+++ ant_nn/agent.py
@@ -1,8 +1,7 @@
-from ant_nn.agent import Brain
 class Agent:
     """Class representing the ant agent"""
-    def __init__(self):
-        self.brain = Brain()
+    def __init__(self, agent_cls):
+        self.brain = agent_cls()
 
     def update(self):
         pass
