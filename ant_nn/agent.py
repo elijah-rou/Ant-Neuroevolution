5313c059fe1b8709e368330743b737895d5c550d
Elijah Roussos <elijah.rou@gmail.com>
2021-03-29 15:22:00 -0400

    more skeleton

---
+++ ant_nn/agent.py
@@ -0,0 +1,8 @@
+from ant_nn.agent import Brain
+class Agent:
+    """Class representing the ant agent"""
+    def __init__(self):
+        self.brain = Brain()
+
+    def update(self):
+        pass
---
+++ ant_nn/environ/GridCell.py
@@ -0,0 +1,13 @@
+class GridCell:
+    """ Class representing a cell in the environment"""
+    def __init__(
+        self,
+        init_params: dict
+    ):
+        self.pheromone = 0
+        self.active = not init_params["is-wall?"]
+        if self.active:
+            self.food = init_params["food"]
+        else
+            self.food = 0
+            
