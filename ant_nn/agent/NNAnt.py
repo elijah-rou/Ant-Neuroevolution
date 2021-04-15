57508c647062fac4932c987feb38488c6a3cacd4
Elijah Roussos <elijah.rou@gmail.com>
2021-04-15 11:40:31 -0400

    start nn agent

--- ant_nn/agent/NNAnt.py
+++ ant_nn/agent/NNAnt.py
@@ -1,9 +1,29 @@
 from ant_nn.agent import Agent
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+class mish(x):
+    """ Mish Activation Function """
+    return x * torch.tanh(F.softplus(x))
+    
+class Brain(nn.Module):
+    """ Neural Net for the ants. Uses 3 hidden layers. """
+    def __init__(self, input_size, output_size, hidden_size):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc2 == nn.Linear(hidden_size, output_size)
+    
+    def forward(self, x):
+        x = mish(self.fc1(x))
+        x = mish(self.fc2(x))
+        x = self.fc2(x)
+        return x
 
 class NNAnt(Agent):
     def __init__(self):
-        pass
+        self.brain = Brain()
 
     def update(self):
         pass
