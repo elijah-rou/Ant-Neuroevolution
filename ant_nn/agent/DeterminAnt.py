f96e19b05b6dab642947a981ec5a7ec0649b7f45
dcmid <d.claymidkiff@gmail.com>
2021-04-15 11:01:54 -0400

    pre-merge commit

--- ant_nn/agent/DeterminAnt.py
+++ ant_nn/agent/DeterminAnt.py
@@ -4,17 +4,11 @@ class DeterminAnt(Agent):
     def __init__(self):
         super.__init__()
 
-        @abstractmethod
     def update(self):
-        """ Update the Agent's state """
-        raise NotImplementedError
+        pass
 
-    @abstractmethod
     def depositPheromone(self):
-        """ Decide whether to drop pheromone """
-        raise NotImplementedError
+        pass
 
-    @abstractmethod
     def move(self):
-        """ Decide a direction to move """
-        raise NotImplementedError
+        pass
