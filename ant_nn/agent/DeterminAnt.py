dfd7d99940a8eb12c53456cee3fd4d405e7dcb3c
dcmid <d.claymidkiff@gmail.com>
2021-04-21 18:45:14 -0400

    added angle lookup table to sense function

--- ant_nn/agent/DeterminAnt.py
+++ ant_nn/agent/DeterminAnt.py
@@ -14,7 +14,25 @@ class DeterminAnt(Agent):
         self.move()
 
     def self.sense(env):
-        cell_pos = self.position.astype(np.uint16)
+        cell_pos = self.get_coord()  # integer coordinates of current cell
+        angle_case = np.round(8 * self.orientation / (2*np.pi)).astype(np.uint8)  # split angles 0-2pi into 8 possible cases
+                                                                                  # really case 0 and 8 are equivalent
+        sense_dict = {
+            #   || LEFTER |  LEFT  | AHEAD |  RIGHT  | RIGHTER || RADIANS
+            8 :  [[ 0, 1], [ 1, 1], [ 1, 0], [ 1,-1], [ 0,-1]],  # 0
+            0 :  [[ 0, 1], [ 1, 1], [ 1, 0], [ 1,-1], [ 0,-1]],
+            1 :  [[-1, 1], [ 0, 1], [ 1, 1], [ 1, 0], [ 1,-1]],  # pi/4
+            2 :  [[-1, 0], [-1, 1], [ 0, 1], [ 1, 1], [ 1, 0]],  # pi/2
+            3 :  [[-1,-1], [-1, 0], [-1, 1], [ 0, 1], [ 1, 1]],  # 3pi/4
+            4 :  [[ 0,-1], [-1,-1], [-1, 0], [-1, 1], [ 0, 1]],  # pi
+            5 :  [[ 1,-1], [ 0,-1], [-1,-1], [-1, 0], [-1, 1]],  # 5pi/4
+            6 :  [[ 1, 0], [ 1,-1], [ 0,-1], [-1,-1], [-1, 0]],  # 3pi/2
+            7 :  [[ 1, 1], [ 1, 0], [ 1,-1], [ 0,-1], [-1,-1]],  # 7pi/4
+        }
+
+        sens_coords = sense_dict[angle_case]  # get indices of sensed cells
+        self.sensed_cells = env[sense_coords[:,0], sens_coords[:,1]]
+        
 
     def depositPheromone(self):
         if self.has_food:
