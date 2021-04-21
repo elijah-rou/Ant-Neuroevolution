d53271615b72dbe33b96ae084240d613d7590915
dcmid <d.claymidkiff@gmail.com>
2021-04-21 19:03:11 -0400

    put NotImplementedErrors back, working on dropping food (should it be stored differently in nest?)

--- ant_nn/agent/DeterminAnt.py
+++ ant_nn/agent/DeterminAnt.py
@@ -1,7 +1,22 @@
-import Agent
+from Agent import Agent
 import numpy as np
 
 class DeterminAnt(Agent):
+
+    # dictionary for looking up indices of sensed cells
+    sense_dict = {
+    #   || LEFTER |  LEFT  | AHEAD |  RIGHT  | RIGHTER || RADIANS
+        8 :  [[ 0, 1], [ 1, 1], [ 1, 0], [ 1,-1], [ 0,-1]],  # 0
+        0 :  [[ 0, 1], [ 1, 1], [ 1, 0], [ 1,-1], [ 0,-1]],
+        1 :  [[-1, 1], [ 0, 1], [ 1, 1], [ 1, 0], [ 1,-1]],  # pi/4
+        2 :  [[-1, 0], [-1, 1], [ 0, 1], [ 1, 1], [ 1, 0]],  # pi/2
+        3 :  [[-1,-1], [-1, 0], [-1, 1], [ 0, 1], [ 1, 1]],  # 3pi/4
+        4 :  [[ 0,-1], [-1,-1], [-1, 0], [-1, 1], [ 0, 1]],  # pi
+        5 :  [[ 1,-1], [ 0,-1], [-1,-1], [-1, 0], [-1, 1]],  # 5pi/4
+        6 :  [[ 1, 0], [ 1,-1], [ 0,-1], [-1,-1], [-1, 0]],  # 3pi/2
+        7 :  [[ 1, 1], [ 1, 0], [ 1,-1], [ 0,-1], [-1,-1]],  # 7pi/4
+    }
+
     def __init__(self):
         super.__init__()
 
@@ -14,23 +29,14 @@ class DeterminAnt(Agent):
         self.move()
 
     def self.sense(env):
+        """Updates current and sensed cells """
+        
         cell_pos = self.get_coord()  # integer coordinates of current cell
         angle_case = np.round(8 * self.orientation / (2*np.pi)).astype(np.uint8)  # split angles 0-2pi into 8 possible cases
                                                                                   # really case 0 and 8 are equivalent
-        sense_dict = {
-            #   || LEFTER |  LEFT  | AHEAD |  RIGHT  | RIGHTER || RADIANS
-            8 :  [[ 0, 1], [ 1, 1], [ 1, 0], [ 1,-1], [ 0,-1]],  # 0
-            0 :  [[ 0, 1], [ 1, 1], [ 1, 0], [ 1,-1], [ 0,-1]],
-            1 :  [[-1, 1], [ 0, 1], [ 1, 1], [ 1, 0], [ 1,-1]],  # pi/4
-            2 :  [[-1, 0], [-1, 1], [ 0, 1], [ 1, 1], [ 1, 0]],  # pi/2
-            3 :  [[-1,-1], [-1, 0], [-1, 1], [ 0, 1], [ 1, 1]],  # 3pi/4
-            4 :  [[ 0,-1], [-1,-1], [-1, 0], [-1, 1], [ 0, 1]],  # pi
-            5 :  [[ 1,-1], [ 0,-1], [-1,-1], [-1, 0], [-1, 1]],  # 5pi/4
-            6 :  [[ 1, 0], [ 1,-1], [ 0,-1], [-1,-1], [-1, 0]],  # 3pi/2
-            7 :  [[ 1, 1], [ 1, 0], [ 1,-1], [ 0,-1], [-1,-1]],  # 7pi/4
-        }
-
         sens_coords = sense_dict[angle_case]  # get indices of sensed cells
+
+        self.current_cell = env[cell_pos]
         self.sensed_cells = env[sense_coords[:,0], sens_coords[:,1]]
         
 
