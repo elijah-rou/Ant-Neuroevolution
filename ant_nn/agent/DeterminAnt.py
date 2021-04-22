1055ea77f1f21a312509753be504a31153d5c018
dcmid <d.claymidkiff@gmail.com>
2021-04-22 10:30:54 -0400

    removed debugging prints

--- ant_nn/agent/DeterminAnt.py
+++ ant_nn/agent/DeterminAnt.py
@@ -36,7 +36,6 @@ class DeterminAnt(Agent):
         grid
         self.sense(grid)
         if(self.current_cell.is_nest):
-            print('IN NEST')
 
         self.pickupFood()
         self.dropFood()
@@ -92,7 +91,6 @@ class DeterminAnt(Agent):
 
         # if food adjacent, head to it
         elif self.adjacent_food > -1 :
-            print('Turning to Food')
             turn = np.pi/2 - self.adjacent_food * (np.pi / 4)
             self.orientation = (self.orientation + turn) % (2 * np.pi)
             self.speed = self.MAX_SPEED
@@ -101,16 +99,13 @@ class DeterminAnt(Agent):
         elif self.current_cell.pheromone > 0.1:
             facing_nest = np.abs(self.orientation - self.get_angle_to_nest()) < np.pi/2
             if facing_nest:
-                print('FACING NEST')
                 self.orientation = (self.orientation + np.pi) % (2 * np.pi)  # turn away from nest
                 self.speed = 0
             elif self.adjacent_pheromone > -1 :
-                print('FOLLOWING PH')
                 turn = np.pi/2 - self.adjacent_pheromone * (np.pi / 4)
                 self.orientation = (self.orientation + turn) % (2 * np.pi)
                 self.speed = self.MAX_SPEED
             else:
-                print('sad')
                 self.orientation = (self.orientation + np.random.normal(0, 0.5)) % (2 * np.pi)
 
         # if pheromone adjacent, head to it
