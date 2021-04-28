8c19054db694ee0836367809c22aa5273baaaa86
Russell Bingham <rbingham97@gmail.com>
2021-04-28 12:22:11 -0400

    ready for testing

--- GUI.py
+++ GUI.py
@@ -54,7 +54,7 @@ class Board(QtWidgets.QFrame):
         super(Board, self).__init__()
 
         self.timer = QtCore.QBasicTimer()
-        self.environ = Environment(h=Board.BoardHeight, w=Board.BoardWidth, agentType="default")
+        self.environ = Environment(h=Board.BoardHeight, w=Board.BoardWidth, agentType="DominAnt")
 
         self.setFocusPolicy(QtCore.Qt.StrongFocus)
 
