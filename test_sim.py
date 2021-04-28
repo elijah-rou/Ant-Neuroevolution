43063592736d1a12f0a712f65caa7f1cf2ee742f
Elijah Roussos <elijah.rou@gmail.com>
2021-04-29 00:18:28 +0200

    reimplement simulation.py as a multi-env runner

--- test_sim.py
+++ test_sim.py
@@ -1,9 +1,14 @@
 from ant_nn.simulation import Simulation
-
+import dill
 
 def main():
-    sim = Simulation(setup="domi")
-    sim.sample_experiment()
+    sim = Simulation()
+    best_chromosomes, best_scores = sim.run()
+
+    file = open("results.pkl", "wb")
+    dill.dump([best_chromosomes, best_chromosomes], file)
+    file.close()
+    print(best_scores)
     print("done")
 
 
