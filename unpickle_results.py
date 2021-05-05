1a22c48be5e83aa5b8da3d85c08a3be0b27618ff
Elijah Roussos <elijah.rou@gmail.com>
2021-05-05 12:26:07 +0200

    get the best chrom

--- unpickle_results.py
+++ unpickle_results.py
@@ -1,5 +1,10 @@
 import pickle
+import numpy as np
 
 pickle_off = open("results.pkl", "rb")
 emp = pickle.load(pickle_off)
 print(emp)
+
+def get_best(emp):
+    best_index = np.argmax(np.max(emp[1], axis=1))
+    return emp[0][best_index][np.argmax(emp[1], axis=1)[best_index]]
