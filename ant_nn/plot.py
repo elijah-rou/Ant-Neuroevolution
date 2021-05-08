a2a66f963fa21f9015e29daed4bfe8e87d81286e
dcmid <d.claymidkiff@gmail.com>
2021-05-08 12:36:37 -0400

    added plot lib stuff

---
+++ ant_nn/plot.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle as pk
+
+def res_to_dict(result):
+    res_dict = {
+    "chromosomes"   :   fetchant_1_temp[0],
+    "scores"        :   np.asarray(fetchant_1_temp[1])
+    }
+    return res_dict
+
+def plot_score_evolution(result):
+    """ plots best score vs epoch """
+
+    res_dict = res_to_dict(result)
+    best_scores = np.max(res_dict['scores'],axis=1)
+
+    plt.plot(best_scores)
+    plt.xlabel('epoch')
+    plt.ylabel('best score')
+    plt.title('Score Evolution')
+    plt.show()
+
+def plot_food_over_time(scores):
+    """ plots best food_gathered vs time for each epoch """
+    pass
+
