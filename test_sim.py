248a29f53a78880acd2a6891c13bf2f30ee260e1
Elijah Roussos <elijah.rou@gmail.com>
2021-05-09 23:00:15 +0000

    ensure feature parity across branches

--- test_sim.py
+++ test_sim.py
@@ -4,10 +4,10 @@ import dill
 
 def main():
     sim = Simulation()
-    chromosomes, scores, final_pop = sim.run()
+    chromosomes, scores, final_pop, food = sim.run()
 
     file = open("results.pkl", "wb")
-    dill.dump([chromosomes, scores, final_pop], file)
+    dill.dump([chromosomes, scores, final_pop, food], file)
     file.close()
     print("done")
 
