307f80f9f90c651a819bf6347fc270c095c88386
dcmid <d.claymidkiff@gmail.com>
2021-05-13 10:15:34 -0400

    added args to sim.run() allowing restarting sim if degen_score isn't reached by degen_epoch

--- ant_nn/simulation.py
+++ ant_nn/simulation.py
@@ -55,71 +55,84 @@ class Simulation:
         self.scores = np.zeros((self.population.size(), self.runs))
         self.food_res = np.zeros((self.population.size(), self.runs, self.TIMESTEPS))
 
-    def run(self):
+    def run(self, degen_epoch=None, degen_score=10):
         """
         Run the simulation
         """
-        e_chromosomes = []
-        final_pop = []
-        pop_size = self.population.size()
-        e_scores = np.zeros((self.epochs, pop_size))
-        # pop_range = range(pop_size)
-        # eval_function = config["eval"]
+        
+        pop_is_degen = True  # used to restart sim if population isn't evolving
+        while (pop_is_degen):
+            e_chromosomes = []
+            final_pop = []
+            pop_size = self.population.size()
+            e_scores = np.zeros((self.epochs, pop_size))
+            max_score = 0
+            # pop_range = range(pop_size)
+            # eval_function = config["eval"]
 
-        for ep in range(self.epochs):
-            t = time.strftime("%X %x %Z")
-            print(f"Generation: {ep+1} - {t}")
+            for ep in range(self.epochs):
+                t = time.strftime("%X %x %Z")
+                print(f"Generation: {ep+1} - {t}")
 
-            future_envs = {
-                self.executor.submit(
-                    sim_env, self.population.chromosomes[i], self.TIMESTEPS, self.config_path
-                ): (i, r)
-                for i in range(pop_size)
-                for r in range(self.runs)
-            }
-            for i, future in enumerate(as_completed(future_envs)):
-                chrom_index, run = future_envs[future]
-                if i % int(0.1 * pop_size * self.runs) == 0 and i != 0:
-                    t = time.strftime("%X %x %Z")
-                    print(f"Completed {i} chromosomes - {t}")
-                try:
-                    food_results = future.result()
-                    self.food_res[chrom_index][run] = food_results
-                    self.scores[chrom_index][run] = food_results[-1]
-                    # print(f"Chromosome {chrom_index}, run {run}: completed {score}")
-                except Exception as e:
-                    print(e)
+                future_envs = {
+                    self.executor.submit(
+                        sim_env, self.population.chromosomes[i], self.TIMESTEPS, self.config_path
+                    ): (i, r)
+                    for i in range(pop_size)
+                    for r in range(self.runs)
+                }
+                for i, future in enumerate(as_completed(future_envs)):
+                    chrom_index, run = future_envs[future]
+                    if i % int(0.1 * pop_size * self.runs) == 0 and i != 0:
+                        t = time.strftime("%X %x %Z")
+                        print(f"Completed {i} chromosomes - {t}")
+                    try:
+                        food_results = future.result()
+                        self.food_res[chrom_index][run] = food_results
+                        self.scores[chrom_index][run] = food_results[-1]
+                        # print(f"Chromosome {chrom_index}, run {run}: completed {score}")
+                    except Exception as e:
+                        print(e)
 
-            # Using executor.map, ignore
-            # sim_args = [c for c in self.population.chromosomes for _ in range(self.runs)]
-            # for chrom_index, score in zip(pop_range, self.executor.map(sim_env, sim_args, chunksize=16)):
-            #     self.scores[chrom_index%self.population.size()] += score
-            #     print(f"Chromosome {chrom_index%self.population.size()}: completed {score}")
+                # Using executor.map, ignore
+                # sim_args = [c for c in self.population.chromosomes for _ in range(self.runs)]
+                # for chrom_index, score in zip(pop_range, self.executor.map(sim_env, sim_args, chunksize=16)):
+                #     self.scores[chrom_index%self.population.size()] += score
+                #     print(f"Chromosome {chrom_index%self.population.size()}: completed {score}")
 
-            if self.eval_function == "median":
-                self.population.scores = np.median(self.scores, axis=1)
-            elif self.eval_function == "median_minvar":
-                self.population.scores = np.median(self.scores, axis=1) - np.std(
-                    self.scores, axis=1
+                if self.eval_function == "median":
+                    self.population.scores = np.median(self.scores, axis=1)
+                elif self.eval_function == "median_minvar":
+                    self.population.scores = np.median(self.scores, axis=1) - np.std(
+                        self.scores, axis=1
+                    )
+                elif eval_function == "median_minvar_ratio":
+                    self.population.scores = np.median(self.scores, axis=1) / np.std(
+                        self.scores, axis=1
+                    )
+                elif eval_function == "average":
+                    self.population.scores = np.mean(self.scores, axis=1)
+                else:
+                    self.population.scores = np.min(self.scores, axis=1)
+                self.population.makeBabies()
+
+                best_index = np.argmax(self.population.scores)
+                e_scores[ep] = self.population.scores
+                best_score = e_scores[ep][best_index]
+                print(
+                    f"Best {self.eval_function} score for epoch {ep+1}: {best_score} - chrom {best_index}\n"
                 )
-            elif eval_function == "median_minvar_ratio":
-                self.population.scores = np.median(self.scores, axis=1) / np.std(
-                    self.scores, axis=1
-                )
-            elif eval_function == "average":
-                self.population.scores = np.mean(self.scores, axis=1)
-            else:
-                self.population.scores = np.min(self.scores, axis=1)
-            self.population.makeBabies()
+                # print(f"Time in thread: {time.thread_time()}\n")
+                e_chromosomes += [self.population.chromosomes[best_index]]
 
-            best_index = np.argmax(self.population.scores)
-            e_scores[ep] = self.population.scores
-            best_score = e_scores[ep][best_index]
-            print(
-                f"Best {self.eval_function} score for epoch {ep+1}: {best_score} - chrom {best_index}\n"
-            )
-            # print(f"Time in thread: {time.thread_time()}\n")
-            e_chromosomes += [self.population.chromosomes[best_index]]
+                max_score = max(max_score,best_score)  # tracks max score over all epochs
+                if (degen_epoch is not None):  # if degen resim is enabled
+                    if (ep+1 >= degen_epoch):  # if past degen_epoch
+                        if (max_score < degen_score):  # if max score across all epochs < degen_score
+                            is_degen = True  # restart the sim
+                            break
+                else:
+                    is_degen = False
 
         final_pop = self.population.chromosomes
 
