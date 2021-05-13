0bd1f5029278e4530e1f1da3de36d4be6b2e96c2
dcmid <d.claymidkiff@gmail.com>
2021-05-13 09:48:16 -0400

    added -c and -r syntax for config/result paths in run_sim

--- test_args.py
+++ test_args.py
@@ -1,41 +1,29 @@
 import sys
 import getopt
-from ant_nn.simulation import Simulation
-import dill
 
-
-def main(cmdline_opts):
+def main( cmdline_opts ):
     try:
-        short_opts = 'c:r:'
         long_opts = ['config=', 'result=']
-        optlist, args = getopt.getopt(cmdline_opts, short_opts, long_opts)
+        optlist, args = getopt.getopt(cmdline_opts, 'c:f:', long_opts)
     except getopt.GetoptError as err:
         # print help information and exit:
         print(err)  # will print something like "option -a not recognized"
         usage()
         sys.exit(2)
 
-    # default paths
+    print(optlist)
+
     config_path = "config.yaml"
     result_path = "result.yaml"
 
-    # parse args
     for opt,arg in optlist:
         if opt in ("-c", "--config"):
             config_path = arg
         if opt in ("-r", "--result"):
             result_path = arg
 
-    print("config file:", config_path)
-    print("result file:", result_path)
-
-    sim = Simulation(config_path=config_path)
-    chromosomes, scores, final_pop, food = sim.run()
-
-    file = open(result_path, "wb")
-    dill.dump([chromosomes, scores, final_pop, food], file)
-    file.close()
-    print("done")
+    print("config:",config_path)
+    print("result:",result_path)
 
 
 if __name__ == "__main__":
